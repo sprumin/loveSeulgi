c3217db234b42257794c8f83b09e986e7ca4a507
sprumin <sprumin123@gmail.com>
2019-02-15 17:36:40 +0900

    complete google image crawler

--- gallery/management/commands/crawl.py
+++ gallery/management/commands/crawl.py
@@ -2,18 +2,44 @@ from django.core.management.base import BaseCommand
 
 from selenium import webdriver
 
+import re
+import time
+
 
 class Command(BaseCommand):
     help = "seulgi image crawler"
 
-    def test_selenium(self, name):
+    def crawl_google_image(self, name):
         driver = webdriver.Chrome("chromedriver")
+        driver.get(f"https://www.google.co.kr/search?q={name}&tbm=isch")
         driver.implicitly_wait(3)
-        params = {
-            "q": name,
-            "tbm": "isch"
-        }
-        driver.get("https://www.google.co.kr/search?", params)
+
+        last_height = driver.execute_script("return document.body.scrollHeight")
+        SCROLL_PAUSE_TIME = 0.5
+
+        while True:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(SCROLL_PAUSE_TIME)
+
+            try:
+                element = driver.find_elements_by_id("smb")[0]
+                element.click()
+            except:
+                pass
+
+            new_height = driver.execute_script("return document.body.scrollHeight")
+
+            if new_height == last_height:
+                break
+
+            last_height = new_height
+
+        images_info = re.findall(r"\"ou\":\"(.*?)\".*?\"pt\":\"(.*?)\".*?\"ru\":\"(.*?)\"", driver.page_source)
+
+        for info in images_info:
+            print(info)
+
+        driver.quit()
 
     def add_arguments(self, parser):
         """ get params """
@@ -25,4 +51,4 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         name = options["name"][0]
 
-        self.test_selenium(name)
+        self.crawl_google_image(name)

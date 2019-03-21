f933e180dc0a4512fa2931288e9ff1bf89bbc7ed
sprumin <sprumin123@gmail.com>
2019-03-21 17:16:33 +0900

    add views thumbs function

--- gallery/views.py
+++ gallery/views.py
@@ -18,6 +18,7 @@ class AlbumView(View):
         if not request.user.is_authenticated:
             return redirect("/user/signin/")
 
+        # send photo data
         if photo_id:
             form = AlbumCommentForm
             photo = Album.objects.get(id=photo_id)
@@ -28,20 +29,37 @@ class AlbumView(View):
                 "photo": photo.photo.url,
                 "title": photo.title,
                 "source": photo.source,
+                "views": photo.views,
+                "thumbs": photo.thumbs,
                 "comment": comment
             }
+
+            # update views
+            photo.views += 1
+            photo.save()
+
             return render(request, "gallery/album.html", {"photo": photo_data, "form": form})
 
         return render(request, "gallery/album.html", get_photos(request))
 
-    def post(self, request, photo_id=None):
+    def post(self, request, photo_id):
+        photo = Album.objects.get(id=photo_id)
+
         username = request.POST.get("username", None)
         comment = request.POST.get("comment", None)
+        thumbs = request.POST.get("thumbs", None)
 
+        # save photo comment
         if username and comment:
-            photo = Album.objects.get(id=photo_id)
-
             album_com = AlbumComment(photo=photo, username=username, message=comment)
             album_com.save()
 
-            return redirect(f"/gallery/album/{photo_id}")
+        # update thumbs
+        # TODO: 일단 횟수제한 없이 세팅하고 프로젝트 완료시 redis에 횟수제한걸기
+        if thumbs:
+            photo.thumbs += 1
+            # thumbs 데이터를 view 로 전송할 때 해당 url에 접근하기때문에 조회수가 두번 누적되어서 한번 빼준다
+            photo.views -= 1
+            photo.save()
+
+        return redirect(f"/gallery/album/{photo_id}")

3e46dd79dc4a19aaa059a6df104de0805f57fa23
root <root@ip-172-31-38-249.us-east-2.compute.internal>
2019-05-23 07:13:13 +0000

    add static files url

--- LoveSeulgi/urls.py
+++ LoveSeulgi/urls.py
@@ -29,4 +29,4 @@ urlpatterns = [
     path("user/", include("user.urls")),
     path("post/", include("post.urls")),
     path("admin/", admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)

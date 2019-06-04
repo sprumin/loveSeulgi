a1b12b4e35a1a71a040e3fa005f928bff386ee1e
sprumin <sprumin@gmail.com>
2019-06-04 10:55:50 +0900

    use semantic-ui modal jquery

--- gallery/views.py
+++ gallery/views.py
@@ -56,6 +56,7 @@ class AlbumView(View):
                 "upload_time": row.created_at,
                 "views": row.views,
                 "thumbs": row.thumbs,
+                "comment": AlbumComment.objects.filter(photo=row),
                 "comments": len(AlbumComment.objects.filter(photo=row))
             })
 

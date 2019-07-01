0e6deb98eefede6eafb46b565dbeb524a2394b8e
sprumin <sprumin@gmail.com>
2019-07-01 16:00:06 +0900

    except instance is None

--- post/signals.py
+++ post/signals.py
@@ -8,15 +8,17 @@ import magic
 
 @receiver(pre_save, sender=Post)
 def post_image_check(sender, instance, *args, **kwargs):
-    mime_type = magic.from_buffer(instance.photo.read(1024), mime=True)
+    if instance.photo:
+        mime_type = magic.from_buffer(instance.photo.read(1024), mime=True)
 
-    if not mime_type.split("/")[0] == "image":
-        raise ValueError("Invalid Image")
+        if not mime_type.split("/")[0] == "image":
+            raise ValueError("Invalid Image")
 
 
 @receiver(pre_save, sender=Report)
 def report_image_check(sender, instance, *args, **kwargs):
-    mime_type = magic.from_buffer(instance.photo.read(1024), mime=True)
+    if instance.photo:
+        mime_type = magic.from_buffer(instance.photo.read(1024), mime=True)
 
-    if not mime_type.split("/")[0] == "image":
-        raise ValueError("Invalid Image")
+        if not mime_type.split("/")[0] == "image":
+            raise ValueError("Invalid Image")

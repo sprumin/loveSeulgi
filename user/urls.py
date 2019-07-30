90af0cb62a32a4a3f9344db398aee4316f5a06e5
sprumin <sprumin123@gmail.com>
2019-07-30 14:08:41 +0900

    remove url last slash

--- user/urls.py
+++ user/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path("signup/", views.UserSignUpView.as_view()),
-    path("signin/", views.UserSignInView.as_view()),
-    path("edit/", views.UserEditView.as_view()),
-    # path("album/", views.UserAlbumView.as_view()),
-    path("signout/", views.signout),
+    path("signup", views.UserSignUpView.as_view()),
+    path("signin", views.UserSignInView.as_view()),
+    path("edit", views.UserEditView.as_view()),
+    # path("album?$", views.UserAlbumView.as_view()),
+    path("signout", views.signout),
 ]
